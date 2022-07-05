ffe005e4799c68f9f8da3cbea43b202803bdcc3c
tarotez <tez@sings.jp>
2022-07-05 19:26:42 +0300

    units shape

--- lib/transform_data_structure.py
+++ lib/transform_data_structure.py
@@ -37,6 +37,7 @@ def renumber_unit_ids_from_global_to_local(units_byc):
         renum = list(map(lambda x: [g2l[x[0]]], units))
         # setting the elements of units_byc_renum to be in the 2-D array shape that will be converted to Matlab matrices
         if len(renum) > 0:
-            units_byc_renum[channel_id] = np.array(renum, dtype=np.int16)[np.newaxis]
+            units_byc_renum[channel_id] = np.array(renum, dtype=np.int16)
+        print('for channel_id', channel_id, ' units_byc_renum[channel_id].shape =', units_byc_renum[channel_id].shape)
         
     return units_byc_renum
