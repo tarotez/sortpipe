238a3d7ce763c1b21ed2ce14e41d87b91aea700f
tarotez <tez@sings.jp>
2022-07-07 17:11:37 +0300

    automatic pipeline from kilosort to plexon offline sorter

--- k2p.py
+++ k2p.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy.io import savemat
 from mainfunc.converter import convert
 from lib.manage_params import read_config
@@ -10,4 +11,4 @@ for subsession_path in get_unprocessed(params.kilo_sorted_dir, params.plexon_inp
     make_directories(params.plexon_input_dir, subsession_path, sep)    
     in_path = params.kilo_sorted_dir + sep + subsession_path
     out_path = params.plexon_input_dir + sep + subsession_path + sep + params.output_mat_file_name
-    savemat(out_path, convert(in_path, params.sample_rate, int(params.n_electrodes), sep))
+    savemat(out_path, convert(in_path, np.double(params.sample_rate), int(params.n_electrodes), sep))
