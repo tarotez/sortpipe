7db7d4a39743ac1176afe2971b5d3ecc7410a576
tarotez <tez@sings.jp>
2022-07-07 16:14:00 +0300

    automatic pipeline from kilosort to plexon offline sorter

--- lib/manage_params.py
+++ lib/manage_params.py
@@ -4,7 +4,7 @@ def read_config():
 
     params = Bunch()
     params.sep = "/"
-    with open('../config/sortpipe_config.csv') as f:
+    with open('../../config/sortpipe_config.csv') as f:
         for line in f:
             key, val = line.rstrip().split(',')
             params[key.rstrip()] = val.lstrip()
