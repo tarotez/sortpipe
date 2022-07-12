1cb21182458acca9314caf71259a725041c93c34
tarotez <tez@sings.jp>
2022-07-12 16:35:01 +0300

    no transpose of wvf, times, and units

--- k2p.py
+++ k2p.py
@@ -7,8 +7,8 @@ from lib.manage_files import get_unprocessed, make_directories
 params = read_config()
 sep = params.sep
 for subsession_path in get_unprocessed(params.kilo_sorted_dir, params.plexon_input_dir, params.output_mat_file_name, sep):
-    print('subsession_path =', subsession_path)        
-    make_directories(params.plexon_input_dir, subsession_path, sep)    
+    print('subsession_path =', subsession_path)
+    make_directories(params.plexon_input_dir, subsession_path, sep)
     in_path = params.kilo_sorted_dir + sep + subsession_path
     out_path = params.plexon_input_dir + sep + subsession_path + sep + params.output_mat_file_name
     converted = convert(in_path, np.double(params.sample_rate), int(params.n_electrodes), sep)
--- mainfunc/converter.py
+++ mainfunc/converter.py
@@ -33,13 +33,17 @@ def convert(dir_path, sample_rate, n_electrodes, sep):
     wvf_byc, times_byc, units_byc = reorganize_by_prim_elec(waveformsL, spike_times, spike_idsL, primary_electrodeL)
     units_byc = renumber_unit_ids_from_global_to_local(units_byc)
 
+    wvf = np.array(wvf_byc, dtype=object)
+    times = np.array(times_byc, dtype=object) / sample_rate
+    units = np.array(units_byc, dtype=object)
+    return dict(wvf=wvf, times=times, units=units)
+
     # change 1-D numpy array into matlab matrices (2-D).
-    wvf_t = np.array(wvf_byc, dtype=object)[np.newaxis].transpose()
-    times_t = np.array(times_byc, dtype=object)[np.newaxis].transpose() / sample_rate
+    # wvf_t = np.array(wvf_byc, dtype=object)[np.newaxis].transpose()
+    # times_t = np.array(times_byc, dtype=object)[np.newaxis].transpose() / sample_rate
     # print('units_byc =')
     # print(units_byc)
     # print('np.array(units_byc, dtype=object).shape =', np.array(units_byc, dtype=object).shape)
-    units_t = np.array(units_byc, dtype=object)[np.newaxis].transpose()
+    # units_t = np.array(units_byc, dtype=object)[np.newaxis].transpose()
 
-    return dict(times=times_t, wvf=wvf_t, units=units_t)
-    
+    # return dict(times=times_t, wvf=wvf_t, units=units_t)
