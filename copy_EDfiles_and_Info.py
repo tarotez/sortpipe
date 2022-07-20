fd87d502da4f024e171c5174cf8e82cc8a9a4e85
tarotez <tez@sings.jp>
2022-07-20 21:32:10 +0300

    copy EDfiles and Info directories that contains behavioral data

---
+++ copy_EDfiles_and_Info.py
@@ -0,0 +1,13 @@
+from lib.manage_files import get_unprocessed, make_directories
+from lib.manage_params import read_config
+
+params = read_config()
+
+for subsession_path in get_unprocessed(params.plexon_input_dir, params.for_stability_analysis_dir, '.mat'):
+
+    print('subsession_path =', subsession_path)
+    sessionID, subsessionID = subsession_path.split('/')
+    src_path = params.plexon_input_dir + '/' + subsession_path + '/' + sessionID + '.mat'
+    make_directories(params.for_stability_analysis_dir, sessionID)
+    trg_dir = params.for_stability_analysis_dir + '/' + sessionID 
+
