235b7f0bf38e402015e1c678eb60a149a0757701
tarotez <tez@sings.jp>
2022-07-20 21:26:58 +0300

    makes session directory only, and not subsession directory

--- divide_by_channels.py
+++ divide_by_channels.py
@@ -7,10 +7,9 @@ params = read_config()
 for subsession_path in get_unprocessed(params.plexon_input_dir, params.for_stability_analysis_dir, '.mat'):
 
     print('subsession_path =', subsession_path)
-    make_directories(params.for_stability_analysis_dir, subsession_path)
-    
     sessionID, subsessionID = subsession_path.split('/')
     src_path = params.plexon_input_dir + '/' + subsession_path + '/' + sessionID + '.mat'
+    make_directories(params.for_stability_analysis_dir, sessionID)
     trg_dir = params.for_stability_analysis_dir + '/' + sessionID + '/elc_01plx'
 
     # orig_data = hdf5storage.loadmat(orig_path, format='7.3', oned_as='column')
--- p2s.py
+++ p2s.py
@@ -7,8 +7,8 @@ params = read_config()
 
 for subsession_path in get_unprocessed(params.manually_sorted_dir, params.for_stability_analysis_dir, 'sort.mat'):
 
+    print('subsession_path =', subsession_path)
     src_dir = params.manually_sorted_dir + '/' + subsession_path
-
     sessionID, subsessionID = subsession_path.split('/')
     make_directories(params.for_stability_analysis_dir, sessionID)
     trg_dir = params.for_stability_analysis_dir + '/' + sessionID + '/elc_01plx'
@@ -24,7 +24,7 @@ for subsession_path in get_unprocessed(params.manually_sorted_dir, params.for_st
             orig_electrodeID = elems[1]
             new_electrodeID = str(int(orig_electrodeID) + 1)
             trg_file = sessionID + '_el' + new_electrodeID + '_subsess' + subsessionID + '_sort.mat'
-            print(src_file, '->', trg_file)
+            # print(src_file, '->', trg_file)
             trg_path = trg_dir + '/' + trg_file
-            print(src_path, '->', trg_path)
+            # print(src_path, '->', trg_path)
             sh.copyfile(src_path, trg_path)
