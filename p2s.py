acde179aa8f6203d5abcd03e6b030f9e40b3b681
tarotez <tez@sings.jp>
2022-07-20 18:46:20 +0300

    plexon output to stability analysis

--- p2s.py
+++ p2s.py
@@ -1,28 +1,28 @@
 import shutil as sh
 from os import listdir
-# from lib.manage_params import read_config
+from lib.manage_params import read_config
+from lib.manage_files import get_unprocessed, make_directories
 
-# params = read_config()
-# sep = params.sep
+params = read_config()
+sep = params.sep
+for subsession_path in get_unprocessed(params.manually_sorted_dir, params.for_stability_analysis_dir, params.output_mat_file_name, sep):
 
-# params.plexon_input_dir + sep + subsession_path
-### src_root_dir = 'D:/manuallyRefined_Nana32'
-src_root_dir = '../../manuallyRefined_Nana32'
-trg_root_dir = '../../forStabilityAnalysis_Nana32'
-sessionID = 'n010222'
-subsessionID = '1'
-src_dir = src_root_dir + '/' + sessionID + '/s' + subsessionID
-trg_dir = trg_root_dir + '/' + sessionID + '/s' + subsessionID
+    make_directories(params.for_stability_analysis_dir, subsession_path, sep)
+    src_dir = params.manually_sorted_dir + '/' + subsession_path
+    trg_dir = params.for_stability_analysis_dir + '/' + subsession_path
 
-# for subsession_path in get_unprocessed(params.kilo_sorted_dir, params.plexon_input_dir, params.output_mat_file_name, sep):
-for src_file in listdir(src_dir):
-    src_path = src_dir + '/' + src_file
-    elems = src_file.split('.')[0].split('_')
-    if elems[1] == 'sort':
-        orig_electrodeID = elems[2]
-        new_electrodeID = str(int(orig_electrodeID) + 1)
-        trg_file = sessionID + '_el' + new_electrodeID + '_subsess' + subsessionID + '_sort.mat'
-        # print(src_file, '->', trg_file)
-        trg_path = trg_dir + '/' + trg_file
-        print(src_path, '->', trg_path)
-        sh.copyfile(src_path, trg_path)
+    sessionID, subsessionID = subsession_path.split('/')    
+
+    for src_file in listdir(src_dir):
+        
+        src_path = src_dir + '/' + src_file        
+        elems = src_file.split('.')[0].split('_')
+        print(elems)
+        if elems[1] == 'sort':
+            orig_electrodeID = elems[2]
+            new_electrodeID = str(int(orig_electrodeID) + 1)
+            trg_file = sessionID + '_el' + new_electrodeID + '_subsess' + subsessionID + '_sort.mat'
+            # print(src_file, '->', trg_file)
+            trg_path = trg_dir + '/' + trg_file
+            print(src_path, '->', trg_path)
+            sh.copyfile(src_path, trg_path)
