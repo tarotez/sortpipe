2c510d8a6f2fd6ca94f549615d00ce5e4fb095cf
tarotez <tez@sings.jp>
2022-07-21 10:51:29 +0300

    catches matwrite error and switches to hdf5 savemat

--- k2p.py
+++ k2p.py
@@ -11,7 +11,7 @@ params = read_config()
 for subsession_path in get_unprocessed(params.kilo_sorted_dir, params.plexon_input_dir, '.mat'):
     print('subsession_path =', subsession_path)
     sessionID = subsession_path.split('/')[0]
-    make_directories(params.plexon_input_dir + '/' subsession_path)
+    make_directories(params.plexon_input_dir + '/' + subsession_path)
     in_path = params.kilo_sorted_dir + '/' + subsession_path
     out_path = params.plexon_input_dir + '/' + subsession_path + '/' + sessionID + '.mat'
     converted = convert(in_path, np.double(params.sample_rate), int(params.n_electrodes))
