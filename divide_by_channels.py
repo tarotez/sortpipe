3b563a1ff33c6887406c0366d392e7f61822458b
tarotez <tez@sings.jp>
2022-07-21 11:08:11 +0300

    catches matReadError and switches to hdf5 loadmat

--- divide_by_channels.py
+++ divide_by_channels.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.io import savemat as scipy_savemat
-from scipy.io.matlab.miobase import MatWriteError
+from scipy.io import loadmat as scipy_loadmat
+from scipy.io.matlab.miobase import MatReadError, MatWriteError
 from hdf5storage import savemat as hdf5_savemat
+from hdf5storage import loadmat as hdf5_loadmat
 
 from lib.manage_files import get_unprocessed, make_directories
 from lib.manage_params import read_config
@@ -16,8 +18,10 @@ for subsession_path in get_unprocessed(params.plexon_input_dir, params.for_stabi
     trg_dir = params.for_stability_analysis_dir + '/' + sessionID + '/elc_01plx'
     make_directories(trg_dir)
 
-    # orig_data = hdf5storage.loadmat(orig_path, format='7.3', oned_as='column')
-    orig_data = hdf5storage.loadmat(src_path)
+    try:
+        orig_data = scipy_loadmat(src_path)
+    except MatReadError:
+        orig_data = hdf5_loadmat(src_path)
     wvf = orig_data['wvf']
     times = orig_data['times']
 
