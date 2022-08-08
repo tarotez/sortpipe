bb7cd7cf6d52d8679020348aaeebed79598dfeb9
tarotez <tez@sings.jp>
2022-08-08 16:20:15 +0300

    saves wvf_single_channel and time_single_channel to avoid 4-D tensor

--- divide_by_channels.py
+++ divide_by_channels.py
@@ -30,22 +30,22 @@ for subsession_path in get_unprocessed(params.plexon_input_dir, params.for_stabi
         # print('times_byc[0].shape =', times_byc[0].shape)
         # print('times_byc[1].shape =', times_byc[1].shape)
 
-        n_samples = wvf_byc[0].shape[1]
-        wvf_1by1 = [[np.zeros((0, n_samples), dtype=np.double)]]
-        times_1by1 = [[np.zeros((0, 1), dtype=np.double)]]
+        # n_samples = wvf_byc[0].shape[1]
+        # wvf_1by1 = [[np.zeros((0, n_samples), dtype=np.double)]]
+        # times_1by1 = [[np.zeros((0, 1), dtype=np.double)]]
 
         # print('before concatenation:')
         # print('wvf_1by1[0].shape =', wvf_1by1[0].shape)
         # print('times_1by1[0].shape =', times_1by1[0].shape)
 
-        wvf_1by1[0][0] = np.concatenate((wvf_1by1[0][0], wvf_byc[orig_electrodeID]), axis=0)
-        times_1by1[0][0] = np.concatenate((times_1by1[0][0], times_byc[orig_electrodeID]), axis=0)
+        # wvf_1by1[0][0] = np.concatenate((wvf_1by1[0][0], wvf_byc[orig_electrodeID]), axis=0)
+        # times_1by1[0][0] = np.concatenate((times_1by1[0][0], times_byc[orig_electrodeID]), axis=0)
 
         # print('after concatenation:')
         # print('wvf_1by1[0].shape =', wvf_1by1[0].shape)
         # print('times_1by1[0].shape =', times_1by1[0].shape)
 
-        divided = dict(wvf=np.array(wvf_1by1, dtype=np.object), times=np.array(times_1by1, dtype=np.object))
+        divided = dict(wvf_single_channel=wvf_byc[orig_electrodeID], times_single_channel=times_byc[orig_electrodeID])
         new_electrodeID = str(orig_electrodeID + 1)
         trg_fileName = sessionID + '_el' + new_electrodeID + '_subsess' + subsessionID_without_s + '.mat'
         trg_path = trg_dir + '/' + trg_fileName
