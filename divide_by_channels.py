efea7b78fc1ac513a75df1222b89a3112c41745b
tarotez <tez@sings.jp>
2022-08-08 12:38:27 +0300

    formatting of wvf_1by1 which saves each channel separately

--- divide_by_channels.py
+++ divide_by_channels.py
@@ -36,10 +36,17 @@ for subsession_path in get_unprocessed(params.plexon_input_dir, params.for_stabi
         wvf_1by1 = [np.zeros((0, n_samples))]
         times_1by1 = [np.zeros((0, n_samples), dtype=np.double)]
 
+        print('before concatenation:')
+        print('wvf_1by1[0].shape =', wvf_1by1[0].shape)
+        print('wvf_1by1[1].shape =', wvf_1by1[1].shape)
+        print('times_1by1[0].shape =', times_1by1[0].shape)
+        print('times_1by1[1].shape =', times_1by1[1].shape)
+
         # np.concatenate((wvf_byc[prim_elec], waveforms), axis=0)
         wvf_1by1[0] = np.concatenate((wvf_1by1[0], wvf_byc[orig_electrodeID]), axis=0)
         times_1by1[0] = np.concatenate((times_1by1[0], times_byc[orig_electrodeID]), axis=0)
 
+        print('after concatenation:')
         print('wvf_1by1[0].shape =', wvf_1by1[0].shape)
         print('wvf_1by1[1].shape =', wvf_1by1[1].shape)
         print('times_1by1[0].shape =', times_1by1[0].shape)
