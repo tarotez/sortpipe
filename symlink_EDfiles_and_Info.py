43aa19ee459c81b56a3b7ed3a3156f8eea574298
tarotez <tez@sings.jp>
2022-08-09 23:11:10 +0300

    copies behavior files

--- symlink_EDfiles_and_Info.py
+++ symlink_EDfiles_and_Info.py
@@ -1,31 +1,31 @@
-from os import symlink
+import shutil as sh
+from os.path import exists
 from lib.manage_files import get_unprocessed, make_directories
 from lib.manage_params import read_config
 
 params = read_config()
 
-edf_suffix = ''
-info_suffix = ''
+src_root = params.behavior_dir
+edf_dirName = 'EDfiles'
+info_dirName = 'Info'
 
-for subsession_path in get_unprocessed(params.plexon_input_dir, params.for_stability_analysis_dir, '.mat'):
+for subsession_path in get_unprocessed(params.behavior_dir, params.for_stability_analysis_dir, ''):
 
     print('subsession_path =', subsession_path)
     sessionID, subsessionID = subsession_path.split('/')
-    src_path = params.plexon_input_dir + '/' + subsession_path + '/' + sessionID + '.mat'
     
-    src_dir = 
+    src_dir = params.behavior_dir + '/' + sessionID + '/'
+    trg_dir = params.for_stability_analysis_dir + '/' + sessionID + '/'
+    # make_directories(trg_dir)
 
-    
+    edf_src = src_dir + edf_dirName
+    edf_trg = trg_dir + edf_dirName
+    info_src = src_dir + info_dirName
+    info_trg = trg_dir + info_dirName
 
-    trg_dir = params.for_stability_analysis_dir + '/' + sessionID 
-    make_directories(trg_dir)
-
-    edf_src = src_dir + edf_suffix
-    edf_trg = trg_dir + edf_suffix
-    info_src = src_dir + info_suffix
-    info_trg = trg_dir + info_suffix
-
-    symlink(edf_src, edf_trg)
-    symlink(info_src, info_trg)
+    if not exists(edf_trg):
+        sh.copytree(edf_src, edf_trg)
+    if not exists(info_trg):
+        sh.copytree(info_src, info_trg)
 
     
