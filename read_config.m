bb1a46306a1c398f0c63e8f8a91676b4cec658bf
tarotez <tez@sings.jp>
2022-08-09 17:58:38 +0300

    symlink behavior files (EDF and Info)

---
+++ read_config.m
@@ -0,0 +1,10 @@
+function config_dict = read_config()
+    fID = fopen('../config/sortpipe_config.csv');
+    config_dict = containers.Map;
+    while ~feof(fID)
+       line = fgetl(fID);
+       split = regexp(line, ', ', 'split');
+       key = split{1};
+       value = split{2};
+       config_dict(key) = value;
+    end
