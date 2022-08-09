0e8e5a1142301f1f7ffc5469d9bb88f7b019b841
tarotez <tez@sings.jp>
2022-08-09 17:38:01 +0300

    reads data/sort_config.csv to find the data directory paths

--- convert_to_1x1_cell_array.m
+++ convert_to_1x1_cell_array.m
@@ -1,5 +1,15 @@
 
-src_dir = "../../data/forStabilityAnalysis_Nana32";
+fID = fopen('../config/sortpipe_config.csv');
+config_dict = containers.Map;
+while ~feof(fID)
+   line = fgetl(fID);
+   split = regexp(line, ', ', 'split');
+   key = split{1};
+   value = split{2};
+   config_dict(key) = value;
+end
+
+src_dir = config_dict('for_stability_analysis_dir');
 sess_filenames = {dir(src_dir).name};
 
 for i = 4:size(sess_filenames,2)
