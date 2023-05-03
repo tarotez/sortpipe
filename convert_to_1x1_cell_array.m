bd3b444b1dc80124e7152cd0b6537a65ec09f995
maru-t <i12isimaru@yahoo.co.jp>
2023-05-03 20:53:17 +0400

    loop with subsessID

--- convert_to_1x1_cell_array.m
+++ convert_to_1x1_cell_array.m
@@ -1,4 +1,3 @@
-
 config_dict = read_config;
 
 src_dir = config_dict('matrix_not_cell_array_dir');
@@ -6,27 +5,33 @@ target_dir = config_dict('for_stability_analysis_dir');
 %%% sess_filenames = {dir(src_dir).name};
 src_dir_struct = dir(src_dir);
 sess_filenames = {src_dir_struct.name};
-
-for i = 4:size(sess_filenames,2)
+for i = 1:size(sess_filenames,2)
     sess_filename = sess_filenames{i}
+    if startsWith(sess_filename, ".")
+        continue
+    end
     % src_sess_dir = src_dir + "/" + sess_filename + "/elc_01plx";
     % target_sess_dir = target_dir + "/" + sess_filename + "/elc_01plx";    
     src_sess_dir = src_dir + "/" + sess_filename;
     target_sess_dir = target_dir + "/" + sess_filename;        
     %%% subsess_filenames = {dir(src_sess_dir).name};
     src_sess_dir_struct = dir(src_sess_dir);
-    subsess_filenames = {src_sess_dir_struct.name}        
-    for j = 3:size(subsess_filenames,2)
-        subsess_filename = subsess_filenames{j};
-        %%%
-        subsessID = 
-        %%%
-        src_subsess_dir = src_dir + "/" + sess_filename + "/" + subsessID;
+    subsess_filenames = {src_sess_dir_struct.name}
+    for j = 1:size(subsess_filenames,2)
+        subsess_filename = subsess_filenames{j}
+        if startsWith(subsess_filename, ".")
+            continue
+        end
+        src_subsess_dir = src_dir + "/" + sess_filename + "/" + subsess_filename + "/elc_01plx";
         src_subsess_dir_struct = dir(src_subsess_dir);
-        recording_filenames = {src_sess_dir_struct.name}
-        for k = 1:size(recording_filenames,2)
-            if endsWith(subsess_filename, "_single_channel.mat")
-                src_file_path = src_sess_dir + '/' + subsessID + "/elc_01plx" + subsess_filename;
+        recording_filenames = {src_subsess_dir_struct.name}
+        for k = 1:size(recording_filenames,2)            
+            recording_filename = recording_filenames{k}
+            if startsWith(recording_filename, ".")
+                continue
+            end            
+            if endsWith(recording_filename, "_single_channel.mat")
+                src_file_path = src_sess_dir + '/' + subsessID + "/" + subsess_filename + "/elc_01plx/" + recording_filename;
                 fprintf("%s\n", src_file_path)            
                 load(src_file_path)
                 wvf = cell(1);
